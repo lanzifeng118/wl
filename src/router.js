008ced79d0b2316a19fbcdf9752801a287678818
crystal.chan <crystal.chan@vipshop.com>
2018-06-13 14:41:39 +0800

    修改index文件

--- src/router.js
+++ src/router.js
@@ -15,22 +15,22 @@ const routers = [
         path: 'setting', component: (slv) => require(['./views/setting.vue'], slv),
         children: [
           { path: '/', component: (slv) => require(['./views/setting-list.vue'], slv), meta: { title: '应用配置' } },
-          { path: 'detail', component: (slv) => require(['./views/setting-detail.vue'], slv) }
+          { path: 'detail', meta: { title: '应用配置' }, component: (slv) => require(['./views/setting-detail.vue'], slv) }
         ]
       },
       {
         path: 'appstore', component: (slv) => require(['./views/appstore.vue'], slv),
         children: [
-          { path: '/', component: (slv) => require(['./views/appstore-list.vue'], slv) },
-          { path: 'detail', component: (slv) => require(['./views/appstore-detail.vue'], slv) }
+          { path: '/', meta: { title: '应用商店' }, component: (slv) => require(['./views/appstore-list.vue'], slv) },
+          { path: 'detail', meta: { title: '应用商店' }, component: (slv) => require(['./views/appstore-detail.vue'], slv) }
         ]
       },
-      { path: 'task', component: (slv) => require(['./views/task.vue'], slv) },
+      { path: 'task', meta: { title: '定时任务' }, component: (slv) => require(['./views/task.vue'], slv) },
       { 
         path: 'me', component: (slv) => require(['./views/me.vue'], slv), redirect: { name: 'msg'},
         children: [
-          { path: 'message', component: (slv) => require(['./views/me-message.vue'], slv), name: 'msg'},
-          { path: 'password', component: (slv) => require(['./views/me-password.vue'], slv)}
+          { path: 'message', meta: { title: '消息中心' }, component: (slv) => require(['./views/me-message.vue'], slv), name: 'msg'},
+          { path: 'password', meta: { title: '修改密码' }, component: (slv) => require(['./views/me-password.vue'], slv)}
         ]
       }
       
